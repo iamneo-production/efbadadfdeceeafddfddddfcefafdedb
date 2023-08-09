e80f9f01fe44f12840895d3fcdf70404c7d400b6
Iamneo <techadmin@iamneo.ai>
2023-08-09 05:10:02 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -3,12 +3,12 @@ package com.examly.springapp;
 import org.testng.annotations.Test;
 import java.net.URL;
 
-import org.assertj.core.api.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+import org.testng.Assert; // Import TestNG's Assert class
 
 public class SpringApplicationTests {
 
@@ -18,13 +18,14 @@ public class SpringApplicationTests {
     @BeforeTest
     public void beforeTest() throws Exception {
         // Replace <seleniumhost:port> with correct values
-        String seleniumHost = "http://seleniumhost";
+        String seleniumHost = "seleniumhost"; // Remove the "http://" part
         int seleniumPort = 4444;
-
+        
         // Construct the Selenium Hub URL
         String hubUrl = String.format("http://%s:%d/wd/hub", seleniumHost, seleniumPort);
-
+        
         driver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
+        
         driver.manage().window().maximize();
     }
 
@@ -33,5 +34,13 @@ public class SpringApplicationTests {
         driver.quit();
     }
 
-   
+    @Test
+    public void testSomething() {
+        // Your test code here
+        // Example:
+        driver.get("https://www.example.com");
+        String title = driver.getTitle();
+        String expectedTitle = "Example Domain"; // Corrected the expected title
+        Assert.assertEquals(title, expectedTitle);
+    }
 }
